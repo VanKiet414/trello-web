2d3b6c2b2980b6aeb9f3e49232bfc0ca67be62ef
Van Kiet <vvkiet.vvkiet12c3tcnvn2023@3tcnvn2023gmail.n2023gmailcom>
2025-04-16 22:27:44 +0700

    Dark Light Mode P2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,62 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-//import HomeIcon from '@mui/icons-material/Home'
-//import { pink } from '@mui/material/colors'
+import HomeIcon from '@mui/icons-material/Home'
+import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
-
 import { useColorScheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import Box from '@mui/material'
+
+import LighModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  //setAge(event.target.value);
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <LighModeIcon fontSize='small'/> Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeOutlinedIcon fontSize='small'/> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <DarkModeOutlinedIcon/>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+            <SettingsBrightnessIcon fontSize='small'/> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
@@ -25,6 +76,8 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <ModeSelect/>
+      <hr />
       <ModeToggle />
       <hr/>
       <div>trungquandev</div>
