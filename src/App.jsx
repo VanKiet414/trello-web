a2db8c4cb827dd8d69626e37b6289ba8f5a43575
Van Kiet <vvkiet.vvkiet12c3tcnvn2023@3tcnvn2023gmail.n2023gmailcom>
2025-04-17 15:07:43 +0700

    Code Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
-import Typography from '@mui/material/Typography'
 import { useColorScheme } from '@mui/material/styles'
-import useMediaQuery from '@mui/material/useMediaQuery'
-
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
-import Box from '@mui/material'
-
+import Box from '@mui/material/Box'
 import LighModeIcon from '@mui/icons-material/LightMode'
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import Container from '@mui/material/Container'
+
 
 function ModeSelect() {
   const { mode, setMode } = useColorScheme()
@@ -47,7 +40,6 @@ function ModeSelect() {
           </Box>
         </MenuItem>
         <MenuItem value="system">
-          <DarkModeOutlinedIcon/>
           <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
             <SettingsBrightnessIcon fontSize='small'/> System
           </Box>
@@ -57,40 +49,37 @@ function ModeSelect() {
   )
 }
 
-
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme()
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-        /* localStorage.setItem('trello-dark-light-mode')
-        localStorage.getItem('trello-dark-light-mode') */
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  )
-}
-
 function App() {
   return (
-    <>
-      <ModeSelect/>
-      <hr />
-      <ModeToggle />
-      <hr/>
-      <div>trungquandev</div>
-
-      <Typography variant="body2" color="text.secondary">Text Typography</Typography>
-
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <br/>
-      <AccessAlarmIcon/>
-      <ThreeDRotation/>
-    </>
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+      <Box sx= {{
+        backgroundColor: 'primary.light',
+        width: '100%',
+        height: (theme) => theme.trello.appBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        <ModeSelect/>
+      </Box>
+      <Box sx= {{
+        backgroundColor: 'primary.dark',
+        width: '100%',
+        height: (theme) => theme.trello.boardBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        Board Bar
+      </Box>
+      <Box sx={{
+        backgroundColor: 'primary.main',
+        width: '100%',
+        height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        Board Content
+      </Box>
+    </Container>
   )
 }
 
