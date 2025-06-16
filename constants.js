36c2f4f221676395b4c09c241bfc421282b3694e
VanKiet414 <vvkiet.12c3tcnvn2023@gmail.com>
2025-06-17 03:04:30 +0700

    Update Domain

---
+++ constants.js
@@ -0,0 +1,11 @@
+// Những cái domain được phép truy cập tới tài nguyên của Server
+export const WHITELIST_DOMAINS = [
+  // 'http://localhost:5173' // Không cần localhost nữa vì ở file config/cors đã luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
+  'https://www.kietdev-trello-web.com'
+  // ...vv vì sau này sẽ deploy lên domain chính thức ...vv
+]
+
+export const BOARD_TYPES = {
+  PUBLIC: 'public',
+  PRIVATE: 'private'
+}
