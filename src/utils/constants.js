5df043bb6759d6863abc06501d656cca2d8a05f9
Van Kiet <vvkiet.vvkiet12c3tcnvn2023@3tcnvn2023gmail.n2023gmailcom>
2025-06-17 02:49:52 +0700

    Update Hosting p1.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,6 +10,7 @@ if (process.env.BUILD_MODE === 'production') {
   apiRoot= 'https://trello-api-epsq.onrender.com'
 }
 
-console.log('🚀 ~ apiRoot:', apiRoot)
+console.log('apiRoot:', apiRoot)
+console.log('apiRoot:', apiRoot)
 
 export const API_ROOT = apiRoot
