8723ea38d784da209869bb26148bd73d65c3861c
vankietdev <vvkiet.12c3tcnvn2023@gmail.com>
2025-07-05 00:38:20 +0700

    Lesson 01

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import Box from '@mui/material/Box'
 import ModeSelect from '~/components/ModeSelect/ModeSelect'
 import AppsIcon from '@mui/icons-material/Apps'
-import { ReactComponent as TrelloIcon } from '~/assets/trello.svg'
-import SvgIcon from '@mui/material/SvgIcon'
+/* import { ReactComponent as TrelloIcon } from '~/assets/trello.svg'
+import SvgIcon from '@mui/material/SvgIcon' */
 import Typography from '@mui/material/Typography'
 import Workspaces from './Menus/Workspaces'
 import Recent from './Menus/Recent'
@@ -20,6 +20,8 @@ import LibraryAddIcon from '@mui/icons-material/LibraryAdd'
 import InputAdornment from '@mui/material/InputAdornment'
 import SearchIcon from '@mui/icons-material/Search'
 import CloseIcon from '@mui/icons-material/Close'
+//import trelloLogo from '~/assets/trello-logo-blue.png'
+import vinLogo from '~/assets/vin-logo.png'
 
 function AppBar() {
   const [searchValue, setSearchValue] = useState ('')
@@ -33,15 +35,16 @@ function AppBar() {
       gap: 2,
       paddingX: 2,
       overflowX: 'auto',
-      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#2c3e50' : '#1565c0'),
+      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#2c3e50' : '#7A8B22'),
       '&::-webkit-scrollbar-track': { m: 2 }
     }}>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <AppsIcon sx={{ color: 'white' }}/>
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
-          <SvgIcon component={TrelloIcon} fontSize='small' inheritViewBox sx={{ color: 'white' }} />
-          <Typography variant="span" sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'white' }}>
-          Trello
+          {/* <SvgIcon component={TrelloIcon} fontSize='small' inheritViewBox sx={{ color: 'white' }} /> */}
+          <img src={vinLogo} alt="Logo" style={{ height: 80, width: 80, objectFit: 'contain' }} />
+          <Typography variant="span" sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white' }}>
+          Vingroup
           </Typography>
         </Box>
 
