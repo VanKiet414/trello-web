f3881cffecf1c6a570cd95df453520dc69e32556
vankietdev <vvkiet.12c3tcnvn2023@gmail.com>
2025-07-14 16:45:06 +0700

    Lesson 06

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 // Những cái domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173' // Không cần localhost nữa vì ở file config/cors đã luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
-  'https://www.kietdev-trello-web.com'
+  'https://api.vankietdev.io.vn'
   // ...vv vì sau này sẽ deploy lên domain chính thức ...vv
 ]
 
