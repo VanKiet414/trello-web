8c1595559a51f41788b6b0c283a075acbbdc7872
vankietdev <vvkiet.12c3tcnvn2023@gmail.com>
2025-07-17 12:51:39 +0700

    Lesson 08

--- src/utils/authorizeAxios.js
+++ src/utils/authorizeAxios.js
@@ -1,6 +1,17 @@
 import axios from 'axios'
 import { toast } from 'react-toastify'
 import { interceptorLoadingElements } from '~/utils/formatters'
+import { refreshTokenAPI } from '~/apis'
+import { logoutUserAPI } from '~/redux/user/userSlice'
+
+/**
+ * Không thể import { store } from '~/redux/store' theo cách thông thường ở đây
+ * Giải pháp: Inject store - là kỹ thuật khi cần sử dụng biến redux store ở các file ngoài phạm vi component như file authorizeAxios hiện tại
+ * Hiểu đơn giản: khi ứng dụng bắt đầu chạy lên, code sẽ chạy vào main.jsx đầu tiên, từ bên đó chúng ta gọi hàm injectStore ngay lập tức để gán biến mainStore vào biến axiosReduxStore cục bộ trong file này.
+ * https://redux.js.org/faq/code-structure#how-can-i-use-the-redux-store-in-non-component-files
+ */
+let axiosReduxStore
+export const injectStore = mainStore => { axiosReduxStore = mainStore }
 
 // Khởi tạo một đối tượng Axios (authorizedAxiosInstance) mục đích để custom và cấu hình chung cho dự án
 let authorizedAxiosInstance = axios.create()
@@ -27,6 +38,12 @@ authorizedAxiosInstance.interceptors.request.use((config) => {
   return Promise.reject(error)
 })
 
+// Khởi tạo một cái promise cho việc gọi api refresh_token
+// Mục đích tạo Promise này để khi nào gọi api refresh_token xong xuôi thì mới retry lại những api
+// bị lỗi trước đó
+// https://www.thedutchlab.com/en/insights/using-axios-interceptors-for-refreshing-your-api-token
+let refreshTokenPromise = null
+
 // Interceptors Response: cho phép can thiệp vào giữa những cái response nhận về
 authorizedAxiosInstance.interceptors.response.use((response) => {
   // Kỹ thuật chặn spam click (xem kĩ mô tả ở formatters chứa function)
@@ -40,6 +57,53 @@ authorizedAxiosInstance.interceptors.response.use((response) => {
 
   // Kỹ thuật chặn spam click (xem kĩ mô tả ở formatters chứa function)
   interceptorLoadingElements(false)
+
+  /* Quan trọng: Xử lý Refresh Token tự động */
+  // Trường hợp 1: Nếu nhận mã 401 từ BE, thì gọi api đăng xuất luôn
+  if (error.response?.status === 401) {
+    axiosReduxStore.dispatch(logoutUserAPI(false))
+  }
+
+  // Trường hợp 2: Nếu nhận mã 410 từ BE, thì sẽ gọi api refresh token để làm mới accessToken
+  // Đầu tiên lấy được các request API đang bị lỗi thông qua error.config
+  const originalRequests = error.config
+  // console.log('originalRequests: ', originalRequests)
+
+  if (error.response?.status === 410 && !originalRequests._retry) {
+    // Gán them _retry = true trong khoảng thời gian chờ, đảm bảo việc refresh token này chỉ luôn gọi 1 lần tại 1 thời điểm (nhìn tại điều kiện if ngay phía trên)
+    originalRequests._retry = true
+
+    // Kiểm tra xem nếu chưa có refreshTokenPromise thì thực hiện gán việc gọi api refresh_token đồng thời gán vào cái refreshTokenPromise
+    if (!refreshTokenPromise) {
+      refreshTokenPromise = refreshTokenAPI()
+        .then(data => {
+          // đồng thời accessToken đã nằm trong httpOnly cookie (xử lý từ phía BE)
+          return data?.accessToken
+        })
+        .catch(() => {
+          // Nếu nhận bất kỳ lỗi nào từ api refresh token thì cứ logout luôn
+          axiosReduxStore.dispatch(logoutUserAPI(false))
+        })
+
+        .finally(() => {
+          // Dù API có thành công hay lỗi thì vẫn luôn gán cái refreshTokenPromise về null như ban đầu
+          refreshTokenPromise = null
+        })
+    }
+
+    // Cần return trong trường hợp refreshTokenPromise chạy thành công và thêm vào đây:
+    // eslint-disable-next-line no-unused-vars
+    return refreshTokenPromise.then(accessToken => {
+      /**
+       * Bước 1: Đối với trường hợp nếu dự án cần lưu accessToken vào localStorage hoặc đâu đó thì sẽ viết thêm code xử lý ở đây.
+       * Ví dụ: axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
+       * Hiện tại ở đây không cần bước 1 này vì chúng ta đã đưa accessToken vào cookie (xử lý từ phía BE) sau khi api refreshToken được gọi thành công.
+       */
+
+      // Bước 2: Bước quan trọng: Return lại axios instance của chúng ta kết hợp các originalRequest để gọi lại những api ban đầu bị lỗi
+      return authorizedAxiosInstance(originalRequests)
+    })
+  }
 
   // Xử lý tập trung phần hiển thị thông báo lỗi trả về từ mọi API ở đây (viết code một lần: Clean Code)
   // console.log error ra là sẽ thấy cấu trúc data dẫn tới message tôi như dưới đây
